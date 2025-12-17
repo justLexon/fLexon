f14d5189e2062fae8eda33f84c2de549e064ecca
Lexon Schexnider <otherslexon@gmail.com>
2025-12-16 19:43:57 -0600

    Added postgres db

--- App/Backend/index.js
+++ App/Backend/index.js
@@ -1,5 +1,7 @@
 // express server
 const express = require("express");
+// progress
+const { Pool } = require("pg");
 
 // port 3000
 const app = express();
@@ -8,33 +10,62 @@ const PORT = 3000;
 // use json format
 app.use(express.json());
 
+// postgres connection
+const pool = new Pool({
+  user: "t",
+  host: "t",
+  database: "t",
+  password: "t",
+  port: 5432,
+});
+
+// temp
+app.get("/db_test", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json({ success: true, time: result.rows[0] });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Database connection failed"});
+    }
+});
+
 
 // GET method for endpoint /health
 app.get("/health", (req, res) => {
     res.json({ status: "ok", message: "Backend running" });
 });
 
-// POST method for endpoint /water
-const waterLogs = [];
+// GET method for endpoint /water
+app.get("/water", (req, res) => {
+    res.json({
+        count: waterLogs.length,
+        data: waterLogs
+    });
+});
 
-app.post("/water", (req, res) => {
+// POST method for endpoint /water
+app.post("/water", async (req, res) => {
     const { amount } = req.body;
 
     if(!amount || amount <= 0)  {
         return res.status(400).json({ error: "Invalid water amount" });
     }
 
-    const entry = {
-        amount,
-        timestamp: new Date()
-    };
+    try {
+        const result = await pool.query(
+            "INSERT INTO water_logs (amount) VALUES ($1) RETURNING *",
+            [amount]
+        );
 
-    waterLogs.push(entry);
-
-    res.status(201).json({
-        success: true,
-        waterEntry: entry
-    });
+        res.status(201).json({
+            success: true,
+            data: result.rows[0],
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Failed to save water intake"});
+    }
 });
 
 // POST method for enpoint /weight
