bfc3455bddf4f709ffbe478c84e466e61900bb4b
Lexon Schexnider <otherslexon@gmail.com>
2026-01-11 14:30:30 -0600

    added cookies and jwt support for postman

--- App/Backend/src/middleware/authMiddleware.js
+++ App/Backend/src/middleware/authMiddleware.js
@@ -1,18 +1,20 @@
-// authorization
 const jwt = require("jsonwebtoken");
 
 module.exports = function authMiddleware(req, res, next) {
-    const token = req.cookies.access_token;
+    const authHeader = req.headers.authorization;
+    const token =
+        req.cookies?.access_token ||
+        (authHeader && authHeader.split(" ")[1]);
 
     if (!token) {
-        return res.status(401).json({ error: "Missing token"});
+        return res.status(401).json({ error: "Missing token" });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.userId;
-        next(); // Pass to controller
+        next();
     } catch (err) {
-        return res.status(401).json({ error: "Invalid or expired token"});
+        return res.status(401).json({ error: "Invalid or expired token" });
     }
 };
