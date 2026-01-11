d8796ac9bc039d036c3d9c9c1c75c37837feec07
Lexon Schexnider <otherslexon@gmail.com>
2026-01-11 14:52:29 -0600

    fix 2.o

--- App/Backend/src/routes/weightRoutes/weight.js
+++ App/Backend/src/routes/weightRoutes/weight.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const authMiddleware = require("../../middleware/authMiddleware");
 const { addWeight, getWeight } = require("../../controllers/weightController");
-const { getWeightWithUser } = require("../../controllers/weightService.js");
+const { getWeightWithUser } = require("../../controllers/weightController.js");
 
 router.get("/", authMiddleware, getWeight)
 router.post("/", authMiddleware, addWeight);
