90b1b30bd3e91aebd19c18cf8e174842da3272e7
Lexon Schexnider <otherslexon@gmail.com>
2026-02-08 18:03:18 -0600

    config adj v2

--- App/Web/app/next.config.mjs
+++ App/Web/app/next.config.mjs
@@ -1,8 +1,12 @@
 import path from "path";
+import { fileURLToPath } from "url";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../.."),
+  outputFileTracingRoot: path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "../../.."
+  ),
 };
 
 export default nextConfig;
